0964cc9070981c2fa003ed697a94b0d05f2db5bd
Nhu <gaococonut1199@gmail.com>
2025-02-10 16:06:46 +0700

    Add login signup

--- app/src/main/java/com/example/pizza3ps/LogInActivity.kt
+++ app/src/main/java/com/example/pizza3ps/LogInActivity.kt
@@ -3,19 +3,34 @@ package com.example.pizza3ps
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var logInButton: Button
+    private lateinit var signUpButton: TextView
+    private lateinit var backButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_log_in)
 
-        val backButton: ImageView = findViewById(R.id.back_button)
-        val signUpButton : TextView = findViewById(R.id.sign_up_text)
+        auth = FirebaseAuth.getInstance()
+
+        logInButton = findViewById(R.id.log_in_button)
+        emailEditText = findViewById(R.id.email_input)
+        passwordEditText = findViewById(R.id.password_input)
+        backButton = findViewById(R.id.back_button)
+        signUpButton = findViewById(R.id.sign_up_text)
 
         backButton.setOnClickListener {
             val intent = Intent(this, WelcomeActivity::class.java)
@@ -27,6 +42,27 @@ class LogInActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
 
             startActivity(intent)
+        }
+
+        logInButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Log in successful!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, DashboardActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Error: Incorrect password or email", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
